875b318c13a0413b157f7477bd41717e8603478a
kisabellecortez <kisabellecortez@gmail.com>
2025-05-29 00:42:10 -0400

    90% done past shows pages

--- src/pages/Shows/AcademyAwards.js
+++ src/pages/Shows/AcademyAwards.js
@@ -1,41 +1,33 @@
 import React from 'react'
 import Nav from '../../components/Navbar.js'
 import Footer from "../../components/Footer";
-import headerImage from "../../images/standFor.png";
+import header from "../../images/standFor.png";
 
 const AcademyAwards = () => {
-    const imageCount=15; 
-    const imageUrls = Array.from({ length : imageCount }, (_, i) => `/images/past-shows/enchanted/${i + 1}.png`);
+    const imageCount = 16; 
+    const imageUrls = Array.from({ length : imageCount }, (_, i) => `/images/past-shows/AcademyAwards/${i + 1}.png`);
 
     return(
-        <div>
+        <div className="shows">
             <Nav />
-            <div style={{ paddingTop: "10vh" }}>
 
-            <div className="header-image-section">
-                <img src={headerImage} alt="past shows" className="header-image" />
-                <div className="header-overlay">
-                    <h1>The Academy Awards</h1>
-                    <h2>(2021 - 2022)</h2>
-                </div>
-                </div>
+            <img className="header" src={header} alt="past shows"/>
 
-            <div style={{ paddingTop: "10vh" }}>
-                <div className="section-light">
-                    <h1>The Academy Awards</h1>
-                <p className="intro-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <div className="gallery-grid">
-                    {imageUrls.map((url, i) => (
-                    <img key={i} src={url} alt={`past show ${i + 1}`} className="gallery-img" />
-                    ))}
-                </div>
-                </div>
-                <Footer />
+            <div className="title-container">
+                <h1 className="title">The Academy Awards</h1>
+                <h1 className="year">(2021 - 2022)</h1>
             </div>
+
+            <p>Summary</p>
+
+            <div className="gallery-grid">
+                {imageUrls.map((url, i) => (
+                <img key={i} src={url} alt={`past show ${i + 1}`} className="gallery-img" />
+                ))}
             </div>
-            </div>
+              
+            <Footer />
+        </div>
     )
 }
 
