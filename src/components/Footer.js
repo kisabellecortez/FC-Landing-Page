860f04aee7fc80a139e303fd86016e76adc01f7f
kisabellecortez <kisabellecortez@gmail.com>
2025-07-14 12:46:15 -0400

    linked pages on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,26 @@ const Footer = () => (
   <footer className="custom-footer">
     <div className="footer-grid">
       <div>
-        <h4>ABOUT US</h4>
-        <p>Our Mission</p>
-        <p>Our Charity</p>
-        <p>WE Charity</p>
-        <p>Endorsement</p>
+        <h4>WHAT WE DO</h4>
+        <p><a href='/about-us'>About Us</a></p>
+        <p><a href='/events'>Events</a></p>
+        <p><a href='/sponsorship'>Sponsorships</a></p>
+        <p><a href='/stand-for'>Charities</a></p>
       </div>
       <div>
-        <h4>SPONSORSHIP</h4>
-        <p>How to Sponsor</p>
-        <p>Previous Sponsors</p>
-        <p>Donate Directly</p>
-        <p>Sponsorship Package</p>
-      </div>
-      <div>
-        <h4>EVENTS</h4>
-        <p>2017 - 2018</p>
-        <p>2016 - 2017</p>
-        <p>2015 - 2016</p>
-        <p>2014 - 2015</p>
-        <p>2013 - 2014</p>
-        <p>2012 - 2013</p>
-        <p>2011 - 2012</p>
-        <p>2010 - 2011</p>
+        <h4>SHOWS</h4>
+        <p><a href='/past-shows/arcana'>2024 - 2025</a></p>
+        <p><a href='/past-shows/spun-from-gold'>2023 - 2024</a></p>
+        <p><a href='/past-shows/soul'>2022 - 2023</a></p>
+        <p><a href='/past-shows/academy-awards'>2021 - 2022</a></p>
+        <p><a href='/past-shows/tuesday'>2018 - 2019</a></p>
+        <p><a href='/past-shows/pandoras'>2017 - 2018</a></p>
+        <p><a href='/past-shows/ttd'>2016 - 2017</a></p>
+        <p><a href='/past-shows/fcmas'>2015 - 2016</a></p>
+        <p><a href='/past-shows/enchanted'>2014 - 2015</a></p>
+        <p><a href='/past-shows/cover-to-cover'>2013 - 2014</a></p>
+        <p><a href='/past-shows/directors-cut'>2012 - 2013</a></p>
+        <p><a href='/past-shows/ffc'>2011 - 2012</a></p>
       </div>
       <div>
         <h4>TEAM</h4>
@@ -37,6 +34,13 @@ const Footer = () => (
         <p><a href="/teams/pr">Public Relations</a></p>
         <p><a href="/teams/sponsorship">Sponsorship</a></p>
         <p><a href="/teams/stage">Stage</a></p>
+      </div>
+      <div>
+        <h4>GET IN TOUCH</h4>
+        <p><a href="/contact-us">Contact Us</a></p>
+        <p><a href="https://www.instagram.com/fashionforchange/?hl=en">Intagram</a></p>
+        <p><a href="https://www.facebook.com/fashionforchange/?ref=embed_page#">Facebook</a></p>
+        <p>University of Waterloo, Waterloo, Ontario N2L 3G1</p>
       </div>
     </div>
     <div className="footer-logo">
