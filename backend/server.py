96efc1f7b42a9a16a2603212f9de7e7bd4603942
kisabellecortez <kisabellecortez@gmail.com>
2025-09-15 23:29:46 -0400

    Changed backend logic.

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ import os
 
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
 BUCKET_NAME = "fashion-for-change"
 
@@ -12,38 +12,38 @@ BUCKET_NAME = "fashion-for-change"
 def home():
     return "Backend is running!", 200
 
-# @app.route("/get-photos", methods=["GET"])
+@app.route("/get-photos", methods=["GET"])
 
-# def get_photos():
-#     try:
-#         s3 = boto3.client(
-#             "s3", 
-#             region_name="us-east-2", 
-#             aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
-#             aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-#         )
+def get_photos():
+    try:
+        s3 = boto3.client(
+            "s3", 
+            region_name="us-east-2", 
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
         
-#         response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        response = s3.list_objects_v2(Bucket=BUCKET_NAME)
 
-#         urls = []
+        urls = []
 
-#         for obj in response.get("Contents", []):
-#             key = obj["Key"]
+        for obj in response.get("Contents", []):
+            key = obj["Key"]
 
-#             url = s3.generate_presigned_url(
-#                 "get_object", 
-#                 Params={"Bucket": BUCKET_NAME, "Key": key}, 
-#                 ExpiresIn = 60,
-#             )
+            url = s3.generate_presigned_url(
+                "get_object", 
+                Params={"Bucket": BUCKET_NAME, "Key": key}, 
+                ExpiresIn = 60,
+            )
 
-#             urls.append(url)
+            urls.append(url)
 
-#         return jsonify({"urls": urls})
+        return jsonify({"urls": urls})
 
-#     except Exception as e:
-#         print("Error: ", e)
-#         return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        print("Error: ", e)
+        return jsonify({"error": str(e)}), 500
     
-# if __name__ == "__main__": 
-#     port = int(os.environ.get("PORT", 4000))
-#     app.run(host="0.0.0.0", debug="True")
+if __name__ == "__main__": 
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", debug="True")
