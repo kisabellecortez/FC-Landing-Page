3a9ae54b847a8ff6d4d0703f0d4e3015c15511c8
kisabellecortez <kisabellecortez@gmail.com>
2025-09-15 20:37:52 -0400

    Hardcoded backend url.

--- frontend/src/context/ImagesContext.js
+++ frontend/src/context/ImagesContext.js
@@ -1,13 +1,12 @@
 import React, { createContext, useEffect , useState } from 'react'
 
 export const ImagesContext = createContext([]);
-const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ImagesProvider  = ({ children }) => {
     const [imagesArray, setImagesArray] = useState([]); 
 
     useEffect(() => {
-        fetch(`${apiUrl}/get-photos`)
+        fetch("https://fashion-for-change.com/get-photos")
             .then((res) => res.json())
             .then((data) => setImagesArray(data.urls))
             .catch((err) => console.error(err));
